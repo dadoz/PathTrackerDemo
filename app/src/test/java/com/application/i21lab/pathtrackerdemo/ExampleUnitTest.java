30f9caf8221d1ca92170fb0f8492301596bde11d
davide-ennova <davide.lamanna@ennova.it>
2017-01-24 20:29:28 +0100

    initial commit

---
+++ app/src/test/java/com/application/i21lab/pathtrackerdemo/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.application.i21lab.pathtrackerdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
